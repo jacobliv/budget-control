90ee330be80c7f18b2f1f5a44e43dea9527f3ec6
Jacob Livingston <jacob.m.livingston@gmail.com>
2019-09-27 15:46:51 -0600

    server code

---
+++ src/main/java/com/budgetControlGroup/budgetControl/BudgetControlApplication.java
@@ -0,0 +1,13 @@
+package com.budgetControlGroup.budgetControl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetControlApplication.class, args);
+	}
+
+}
